c2d02cd243a15067cdb22387d5ac1dc521b5803f
jana-alam <janaalam2929@gmail.com>
2021-11-12 11:35:09 +0600

    home page product design

---
+++ src/pages/Home/Products/Products.js
@@ -0,0 +1,30 @@
+import React from "react";
+import SingleProduct from "../SingleProduct/SingleProduct";
+
+const Products = () => {
+  return (
+    // Products section homepage
+    <section className="py-8">
+      <div className="space-y-4">
+        <h1 className="text-center text-6xl text-red-600 font-semibold">
+          Our Scooty
+        </h1>
+        <p className="text-center text-lg font-medium text-gray-700">
+          We have the best quality scooters with 24 hour support accross the
+          country
+        </p>
+      </div>
+      {/* Products container */}
+      <div className="w-10/12 mx-auto grid grid-cols-1 sm:grid-cols-2 gap-8 shadow-2xl mt-8 bg-gray-100">
+        <SingleProduct />
+        <SingleProduct />
+        <SingleProduct />
+        <SingleProduct />
+        <SingleProduct />
+        <SingleProduct />
+      </div>
+    </section>
+  );
+};
+
+export default Products;
---
+++ src/pages/Home/SingleProduct/SingleProduct.js
@@ -0,0 +1,34 @@
+import React from "react";
+import scooty1 from "../../../images/scooty-2.png";
+
+const SingleProduct = () => {
+  return (
+    <div className="flex space-x-6 items-center p-6">
+      <img src={scooty1} className="w-4/12" alt="product" />
+      <div className="text-gray-900">
+        <h2 className="text-3xl font-semibold text-gray-800">
+          Mini Electric Scooter
+        </h2>
+        <p className="text-gray-600 mb-4">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi,
+          iusto.
+        </p>
+        <p className="grid grid-cols-2 font-semibold">
+          <span>Price</span> <span className="text-red-600 ">$600</span>
+        </p>
+        <p className="grid grid-cols-2 font-semibold">
+          <span>Power</span> <span className=" ">250 W</span>
+        </p>
+        <p className="grid grid-cols-2 font-semibold">
+          <span>Availibility</span>{" "}
+          <span className="text-green-600 ">In Stock!</span>
+        </p>
+        <button className="px-6 py-2 mt-4 bg-red-600 text-white rounded-full">
+          Buy It Now
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SingleProduct;
